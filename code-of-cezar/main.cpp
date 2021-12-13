e04fcecb4ba0c0e6a2e6a2c2f222e04a96547d56
mirmicuper <alexander.onthe@gmail.com>
2021-12-13 17:20:22 +0300

    Initial Commit

---
+++ code-of-cezar/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  code-of-cezar
+//
+//  Created by Александр Гемза on 13.12.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
