88b82b5fd8197c4f77cac630fc110f67d7942843
mirmicuper <alexander.onthe@gmail.com>
2021-12-20 15:11:54 +0300

    first commit

---
+++ code-of-cezar/2.cpp
@@ -0,0 +1,107 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+int main(){
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	const int c = 100;
+	char a[c],rez[c];
+	int k, b, len,rot;
+	cout << "введите строку ";
+	cin.getline(a, 100);
+	cout << "ROT ";
+	cin >> rot;
+	cout << "язык 1-англ, 2-рус ";
+	cin >> k;
+	len = strlen(a);
+
+	if (rot == 0) {
+		cout << "Ты дурак? тебе вообще сдвигать надо или нет?";
+		return 0;
+	}
+
+	switch (k) {
+	case 1:
+		if (rot > 0) {
+			for (int i = 0; i < len; i++) {
+				if ((int)a[i] + rot >= 122 && (int)a[i] <= 122 && (int)a[i] >= 97) {
+					int l = (int)a[i] + rot - 122;
+					rez[i] = 96 + l;
+					cout << rez[i];
+				}
+
+				if ((int)a[i] + rot >= 90 && (int)a[i] <= 90 && (int)a[i] >= 65) {
+					int l = (int)a[i] + rot - 90;
+					rez[i] = 64 + l;
+					cout << rez[i];
+				}
+
+				if ((int)a[i] <= 90 && (int)a[i] >= 65 && (int)a[i] + rot <= 90 || (int)a[i] <= 122 && (int)a[i] >= 97 && (int)a[i] + rot <= 122) {
+					rez[i] = (int)a[i] + rot;
+					cout << rez[i];
+				}
+			}
+		}
+		if (rot < 0) {
+			for (int i = 0; i < len; i++) {
+				if ((int)a[i] + rot < 97 && (int)a[i] <= 122 && (int)a[i] >= 97) {
+					int l = 96 - ((int)a[i] + rot);
+					rez[i] = 122 - l;
+					cout << rez[i];
+				}
+				if ((int)a[i] + rot < 65 && (int)a[i] <= 90 && (int)a[i] >= 65) {
+					int l = 64 - ((int)a[i] + rot);
+					rez[i] = 90 - l;
+					cout << rez[i];
+				}
+				if ((int)a[i] <= 90 && (int)a[i] >= 65 && (int)a[i] + rot <= 90 && (int)a[i] + rot >= 65 || (int)a[i] <= 122 && (int)a[i] >= 97 && (int)a[i] + rot <= 122 && (int)a[i] + rot >= 97) {
+					rez[i] = (int)a[i] + rot;
+					cout << rez[i];
+				}
+			}
+		}
+		break;
+	case 2:
+		cout << "буквы ё и Ё не учитываются" << endl;
+		if (rot > 0) {
+			for (int i = 0; i < len; i++) {
+				if ((int)a[i] + rot >= -1 && (int)a[i] >= -32 && (int)a[i] <= -1) {
+					int l = (int)a[i] + rot + 1;
+					rez[i] = -33 + l;
+					cout << rez[i];
+				}
+
+				if ((int)a[i] + rot >= -33 && (int)a[i] >= -64 && (int)a[i] <= -33) {
+					int l = (int)a[i] + rot + 33;
+					rez[i] = -65 + l;
+					cout << rez[i];
+				}
+
+				if ((int)a[i] >= -64 && (int)a[i] <= -33 && (int)a[i] + rot <= -33 || (int)a[i] >= -32 && (int)a[i] <= -1 && (int)a[i] + rot <= -1) {
+					rez[i] = (int)a[i] + rot;
+					cout << rez[i];
+				}
+			}
+		}
+		if (rot < 0) {
+			for (int i = 0; i < len; i++) {
+				if ((int)a[i] + rot < -32 && (int)a[i] >= -32 && (int)a[i] <= -1) {
+					int l = (int)a[i] + rot + 32;
+					rez[i] = l;
+					cout << rez[i];
+				}
+				if ((int)a[i] + rot < -64 && (int)a[i] >= -64 && (int)a[i] <= -33) {
+					int l = (int)a[i] + rot + 64;
+					rez[i] = -32 + l;
+					cout << rez[i];
+				}
+				if ((int)a[i] >= -64 && (int)a[i] <= -33 && (int)a[i] + rot <= -33 && (int)a[i] + rot >= -64 || (int)a[i] >= -32 && (int)a[i] <= -1 && (int)a[i] + rot <= -1 && (int)a[i] + rot >= -32) {
+					rez[i] = (int)a[i] + rot;
+					cout << rez[i];
+				}
+			}
+		}
+		break;
+	}
+	return 0;
+}
---
+++ code-of-cezar/sismssizl.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	const int a = 100;
+	int b,d,k;
+	char c[a];
+	cout << "Введите число в десятичной системе счисления ";
+	cin>>b;
+	_itoa_s(b, c, 2);
+	cout << "число в двоичной системе счисления " << c << endl;
+	_itoa_s(b, c, 8);
+	cout << "число в восимиричной системе счисления "<<c<<endl;
+	_itoa_s(b, c, 10);
+	cout << "число в десятичной системе счисления "<<c<<endl;
+	_itoa_s(b, c, 16);
+	cout << "число в шестнадцатиричной системе счисления "<<c<<endl;
+	cout << "Не та система счисления?1-да 2-нет";
+	cin>>d;
+	switch (d) {
+	case 1:
+		cout << "введите необходимую систему счисления ";
+		cin >> k;
+		_itoa_s(b, c, k);
+		cout << "число в " << k << " системе счисления " << c << endl;
+		break;
+	case 2:
+		cout << "пока";
+		break;
+	}
+	return 0;
+}
